cbb2e2e394c97b5b2b82856df2beb1395ca34b9e
jayesh <jayeshsarvaiya9913@gmail.com>
2026-04-07 19:43:28 +0530

    remove dist

--- dist/dev/static/chunks/app_page_tsx_0wskn30._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_0puu-kf._.js","static/chunks/node_modules_0pa5bcl._.js"],
-    source: "dynamic"
-});
